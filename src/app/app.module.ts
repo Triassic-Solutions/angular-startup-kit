b969674a7c3f156797c5d2343899e3b14ee925bf
Jishnu Sreekantan <jishnutkl@gmail.com>
2021-07-13 17:42:16 +0530

    Updating Project Structure
    Home component added, shell created, core module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShellComponent } from './shell/shell.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { FooterComponent } from './shell/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShellComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
