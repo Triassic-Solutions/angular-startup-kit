eaa4eae918c508886f94b651f26c59cc4bca31c9
Jishnu Sreekantan <jishnutkl@gmail.com>
2021-07-14 20:15:34 +0530

    logger and routing
    home and shell routing (WIP)

--- src/app/@core/logger.service.ts
+++ src/app/@core/logger.service.ts
@@ -3,7 +3,46 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class LoggerService {
 
-  constructor() { }
+export enum LogLevel {
+  OFF = 0,
+  ERROR,
+  WARNING,
+  INFO,
+  DEGUB
+}
+
+export class Logger {
+
+  static logLevel = LogLevel.DEGUB;
+
+  static inProductionMode() {
+    Logger.logLevel = LogLevel.WARNING;
+  }
+
+  debug(...log : any[]) {
+    this.log(console.log, LogLevel.DEGUB, log);
+  }
+
+  error(...log : any[]) {
+    this.log(console.error, LogLevel.ERROR, log);
+  }
+
+  warn(...log : any[]) {
+    this.log(console.warn, LogLevel.WARNING, log);
+  }
+
+  info(...log : any[]) {
+    this.log(console.info, LogLevel.INFO, log);
+  }
+
+  constructor(private component?: string) { }
+
+  private log(logFunction: (...args: any[]) => void, logLevel: LogLevel, log: any[]) {
+    if(logLevel <= Logger.logLevel) {
+
+      const appLog = this.component ? ['[' + this.component + ']'].concat(log) : log;
+      logFunction.apply(console, appLog);
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Logger } from './@core/logger.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-startup-kit';
+
+  ngOnInit() {
+    if(environment.production) {
+      Logger.inProductionMode();
+    }
+  }
 }
